b86bcc1c085f58568a3ba3962353b8cf40cf3745
leowang <rfv7855659@gmail.com>
2022-10-02 02:14:41 +0800

    ✨add base config

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+import { generateDocument } from './doc';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+  app.enableVersioning({
+    defaultVersion: [VERSION_NEUTRAL, '1'],
+    type: VersioningType.URI,
+  });
+  generateDocument(app);
+
   await app.listen(3000);
 }
 bootstrap();
